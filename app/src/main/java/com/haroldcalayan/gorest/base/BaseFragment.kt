869d5ff7a6d1148c2b7047211d02b1a844fc45e2
Harold Calayan <haroldcalayan@gmail.com>
2021-07-10 14:25:43 +0800

    :sparkles: Create base fragment

---
+++ app/src/main/java/com/haroldcalayan/gorest/base/BaseFragment.kt
@@ -0,0 +1,40 @@
+package com.haroldcalayan.gorest.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<T : BaseViewModel, B: ViewDataBinding> : Fragment() {
+
+    abstract val viewModel: T
+    abstract val layoutId: Int
+
+    lateinit var binding: B
+    lateinit var rootView: View
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        binding.lifecycleOwner = this
+        rootView = binding.root
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    open fun initViews() {
+    }
+
+    open fun observe() {
+    }
+}
